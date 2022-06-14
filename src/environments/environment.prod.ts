900b4ab39a898e9a4f2bd6c25f82ef137b83cf37
M.Polverini <m.polverini@almaviva.it>
2022-06-14 21:07:32 +0200

    FIREBASE

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBYqyiCq3OOx16VbcbO6K_fbQ6a_XTM_ts',
+    authDomain: 'ng-datemindr.firebaseapp.com',
+    projectId: 'ng-datemindr',
+    storageBucket: 'ng-datemindr.appspot.com',
+    messagingSenderId: '993658402198',
+    appId: '1:993658402198:web:826626b8de5c5e1bf719b7',
+    measurementId: 'G-5302HTDEYK',
+  },
 };
